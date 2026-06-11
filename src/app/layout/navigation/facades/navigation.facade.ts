a9131a8a17f774dacb04400f216b2744c4de32c9
David Lino <davidelino290@gmail.com>
2026-06-11 15:45:20 -0300

    fix: icons

--- src/app/layout/navigation/facades/navigation.facade.ts
+++ src/app/layout/navigation/facades/navigation.facade.ts
@@ -5,10 +5,10 @@ import { NavItem } from '../models/navigation.model';
 @Injectable({ providedIn: 'root' })
 export class NavigationFacade {
   private readonly _items$ = new BehaviorSubject<NavItem[]>([
-    { label: 'Início', icon: '🏠', route: '' },
-    { label: 'Buscar', icon: '🔍', route: 'explore' },
-    { label: 'Pedidos', icon: '🧾', route: 'orders' },
-    { label: 'Perfil', icon: '👤', route: 'profile' },
+    { label: 'Início', icon: '', route: '' },
+    { label: 'Buscar', icon: '', route: 'explore' },
+    { label: 'Pedidos', icon: '', route: 'orders' },
+    { label: 'Perfil', icon: '', route: 'profile' },
   ]);
   readonly items$: Observable<NavItem[]> = this._items$.asObservable();
 }
